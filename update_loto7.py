a3dd2e0878628f83995880e96ebca98d1d97cef2
hiroto871610-pixel <hiroto871610@gmail.com>
2026-03-29 22:44:40 +0900

    Update update_loto7.py

--- update_loto7.py
+++ update_loto7.py
@@ -1,10 +1,8 @@
 import random
-import re
 
 # 1. ロト7の予想番号（7個）を生成する関数
 def generate_numbers():
     numbers = sorted(random.sample(range(1, 38), 7))
-    # 数字を2桁の文字列にする（例: 3 -> "03"）
     return [str(n).zfill(2) for n in numbers]
 
 # 2. 新しいHTMLのパーツを組み立てる
@@ -16,25 +14,33 @@ for label in labels:
     balls_html = "".join([f'<span class="ball">{n}</span>' for n in nums])
     
     new_html_content += f'''
-    <div class="numbers-row">
-        <div class="row-label">{label}</div>
-        <div class="ball-container">
-            {balls_html}
-        </div>
-    </div>
-    '''
-new_html_content += '</div>'
+                <div class="numbers-row">
+                    <div class="row-label">{label}</div>
+                    <div class="ball-container">
+                        {balls_html}
+                    </div>
+                </div>'''
+new_html_content += '\n            </div>'
 
 # 3. loto7.html を読み込む
 with open('loto7.html', 'r', encoding='utf-8') as file:
     html_data = file.read()
 
-# 4. と の間を、新しいHTMLに差し替える
-pattern = r'()(.*?)()'
-updated_html = re.sub(pattern, rf'\1\n{new_html_content}\n\3', html_data, flags=re.DOTALL)
+# 4. 超安全な書き換えロジック（ハサミで切って間に挟む方式）
+start_marker = ""
+end_marker = ""
 
-# 5. 上書き保存する
-with open('loto7.html', 'w', encoding='utf-8') as file:
-    file.write(updated_html)
-
-print("ロト7の予想番号を自動更新しました！")
+if start_marker in html_data and end_marker in html_data:
+    # 目印より「前」のHTMLと、「後」のHTMLを切り分ける
+    before_html = html_data.split(start_marker)[0]
+    after_html = html_data.split(end_marker)[1]
+    
+    # 前 + 目印 + 新しい予想 + 目印 + 後 で合体させる
+    updated_html = before_html + start_marker + "\n            " + new_html_content + "\n            " + end_marker + after_html
+    
+    # 5. 上書き保存する
+    with open('loto7.html', 'w', encoding='utf-8') as file:
+        file.write(updated_html)
+    print("大成功！デザインを保ったままロト7の予想番号を更新しました！")
+else:
+    print("エラー：loto7.html の中に目印が見つかりません。")
