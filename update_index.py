61b0c0f72373b02f79736c50c245bfde84d5ac91
hiroto871610-pixel <hiroto871610@gmail.com>
2026-04-04 00:40:20 +0900

    Update update_index.py

--- update_index.py
+++ update_index.py
@@ -232,6 +232,7 @@ def build_index_html():
         <a href="loto6.html">ロト6</a>
         <a href="numbers.html">ナンバーズ</a>
         <a href="jumbo.html">ジャンボ</a>
+        <a href="column.html">コラム🔰</a>
     </nav>
 
     <div style="text-align: center; margin: 20px 0;">
